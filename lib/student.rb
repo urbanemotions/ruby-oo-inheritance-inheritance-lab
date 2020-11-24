c23ed4bf40b93a3411bb5bc8aefea1fd114f2048
sin wan wildy <sw.wildy@gmail.com>
2020-11-24 14:23:01 -0800

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./User.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge.append(string)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+    
+    attr_accessor :first_name, :last_name 
+
+    #new_user = User.new("Jim","Newbie")
+end
